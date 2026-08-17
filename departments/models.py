cc2d673548b7dbcb0696585e6f618b2915d93c2f
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-17 12:28:13 +0545

    Add Department Model

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(
+        max_length=20,
+        unique=True,
+
+    )
+    description = models.CharField(
+        max_length=255,
+        blank=True
+    )
+    is_active = models.BooleanField(
+        default=True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    def __str__(self):
+        return self.name
