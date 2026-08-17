70974c153d8ba0e06420bdca91c92cbc4ebbeda4
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-17 15:08:21 +0545

    Add tasks serializer

---
+++ tasks/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "id",
+            "employee",
+            "title",
+            "description",
+            "status",
+            "priority",
+            "due_date",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = [
+            "id",
+            "created_at",
+            "updated_at"
+        ]
