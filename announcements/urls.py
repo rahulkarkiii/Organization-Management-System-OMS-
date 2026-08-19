5346735e684fa7a837c28c434d8400e5557434d3
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-19 11:58:54 +0545

    completed announcements backend

---
+++ announcements/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    AnnouncementListCreateView,
+    AnnouncementDetailView,
+)
+urlpatterns = [
+    path(
+        "",
+        AnnouncementListCreateView.as_view(),
+        name="announcement-list-create",
+    ),
+    path(
+        "<int:pk>/",
+        AnnouncementDetailView.as_view(),
+        name="announcement-detail",
+    )
+
+]
