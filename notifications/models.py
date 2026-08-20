f38688278d91add10756f5984aac0ffb31fe5a3a
rahulkarkiii <rahulkarkeee@gmail.com>
2026-08-20 12:28:00 +0545

    Add Notification Model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Notification(models.Model):
+    NOTIFICATION_TYPES = [
+        ("announcement", "Announcement"),
+        ("task", "Task"),
+        ("leave", "Leave"),
+        ("attendance", "Attendance"),
+        ("system", "System"),
+    ]
+
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="notifications"
+    )
+    title = models.CharField(
+        max_length=255,
+    )
+    message = models.TextField()
+    notification_type = models.CharField(
+        max_length=25,
+        choices=NOTIFICATION_TYPES,
+        default= "system",
+    )
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.recipient}: {self.title}"
+
